459ce0e3a187b8b61eb11b21e7fecb018597567e
Juan Ignacio Hita Manso <alu0100713213@ull.edu.es>
2015-10-19 21:17:41 +0100

    Modificando web worker

--- js/temperature.js
+++ js/temperature.js
@@ -3,7 +3,6 @@ function startWorker(valor) {
     //alert("el valor es:"+valor);
     if(typeof(Worker) !== "undefined") { // check whether the user's browser supports Web Workers
         if(typeof(w) == "undefined") {   // checks if the worker already exists
-          w = new Worker("js/convertidor.js"); // creates a new web worker and runs
           convertir(valor);
         }
         w.onmessage = function(event) {
